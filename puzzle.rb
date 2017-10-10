5cd239603107660c6d5ca54ed1829a7b33f6a5fe
Steven Clontz <steven.clontz@gmail.com>
2017-10-10 14:52:42 -0500

    smaller grid, bigger letters/numbers

--- puzzle.rb
+++ puzzle.rb
@@ -1,23 +1,22 @@
 SOLUTION = <<STRING
----TH---
---E--R--
---E--S--
---U--L--
----TFO--
------L--
---L--O--
----WS---
+--THE--
+-R---E-
+-S---U-
+--LTF--
+-----O-
+-L---L-
+--OWS--
 STRING
 
 SOLUTION.split("\n").each_with_index do |line,row|
   line.split("").each_with_index do |letter,col|
-    random_nine_mult = (51..1111).to_a.shuffle.first*9
+    random_nine_mult = (12..111).to_a.shuffle.first*9
     if letter == "-"
       letter = ('A'..'Z').to_a.shuffle.first
       random_number = random_nine_mult + (1..8).to_a.shuffle.first
     else
       random_number = random_nine_mult
     end
-    puts "\\cellEntry{(#{col},#{7-row})}{#{letter}}{#{random_number}}"
+    puts "\\cellEntry{(#{col},#{6-row})}{#{letter}}{#{random_number}}"
   end
 end
